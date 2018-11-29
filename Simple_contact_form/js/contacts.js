d019540179e8eb2ccbda48300d8ff24e3a7edc44
Mohammad Reza <rza9001@gmail.com>
2018-11-29 22:10:48 +0700

    Update contacts.js

--- Simple_contact_form/js/contacts.js
+++ Simple_contact_form/js/contacts.js
@@ -6,33 +6,36 @@ Activity: Contact manager
 // https://stackoverflow.com/questions/24924032/how-to-loop-if-else-statements-javascript
 // https://stackoverflow.com/questions/40606697/how-do-i-loop-back-to-the-prompt-if-answer-is-incorrect-javascript
 // https://www.freecodecamp.org/forum/t/js-while-loop-problem/56346/5
+// https://stackoverflow.com/questions/36375261/nested-while-loops-in-javascript
 var greetings = "Welcome to your contacts manager!";
-var choice1 = "1: Lists contacts";
-var choice2 = "2: Add a contacts ";
+var choice1 = "1: List contacts";
+var choice2 = "2: Add a contact";
 var choice0 = "0: Quit";
 var lists = "Here's the list of all your contacts:";
 var contacts = [{firstName: 'John', lastName: 'Smith'}, {firstName: 'Jane', lastName:'Doe'}];
-var runContact = true;
-var choicesPrompt = 0;
-var choicesPromptArrayValue;
-var choicesPromptArray = [];
-
+var runContact, choicesPrompt, addFirstName, addLastName, addFirstNameCapitalize, addLastNameCapitalize;
 
 init();
 input();
 
 function input() {
   while(choicesPrompt === 1) {
-        console.log(lists);
-        listContacts();
-        greetingChoices();
-        runChoicesPrompt();
+    console.log(lists);
+    listContacts();
+    greetingChoices();
+    runChoicesPrompt();
+
+    while(choicesPrompt === 2) {
+      addContact();
+      greetingChoices();
+      runChoicesPrompt();
+    }
+
+    if(choicesPrompt === 0) {
+      runContact = false;
+      break;
+    }
   }
-  runContact = false;
-/*   while (choicesPrompt === 0) {
-        // Quit the contact app when the user input 0 for the choice
-        runContact = false;
-      }   */   
 };
 
 function greetingChoices() {
@@ -41,20 +44,27 @@ function greetingChoices() {
   console.log(choice0);
 }
 
+function runChoicesPrompt() {
+  choicesPrompt = parseInt(prompt('Please enter your choice:')); 
+}
+
 function listContacts() {
   for (var i = 0; i < contacts.length; i++) {
     console.log("Last name: " + contacts[i].lastName + ", first name: " + contacts[i].firstName);
   }
 } 
 
-function runChoicesPrompt() {
-  choicesPrompt = parseInt(prompt('Please enter your choice')); 
-  // choicesPromptArray.push(choicesPrompt);
-  // choicesPromptArrayValue = choicesPromptArray[choicesPromptArray.length-1];
-  // console.log(choicesPromptArrayValue);
+function addContact() {
+  addFirstName = String(prompt("Please enter your First Name"));
+  addFirstNameCapitalize = addFirstName.charAt(0).toUpperCase() + addFirstName.slice(1);
+  addLastName = String(prompt("Please enter your Last Name"));
+  addLastNameCapitalize = addLastName.charAt(0).toUpperCase() + addLastName.slice(1);
+  contacts.push({firstName: addFirstNameCapitalize, lastName: addLastNameCapitalize});
+  console.log("Contact added");
 }
 
 function init() {
+  runContact = true;
   console.log(greetings);
   greetingChoices();
   runChoicesPrompt();
